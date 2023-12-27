f2b64ecd47f182d643d5fc1fe5d01248d80a098a
cccs-pg <60066697+cccs-pg@users.noreply.github.com>
2023-12-27 07:10:10 -0500

    Coverter to new API and added file submission to AL

--- cccs-tools/chatbot_cccs.py
+++ cccs-tools/chatbot_cccs.py
@@ -2,6 +2,7 @@ from dotenv import load_dotenv
 import os
 import requests
 import json
+import hashlib
 
 from langchain.agents import initialize_agent, Tool
 from langchain.agents import AgentType
@@ -9,15 +10,18 @@ from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
 from langchain.chains import LLMChain
 from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
 
 load_dotenv('../.env')
 
 AL_API_KEY = os.getenv('AL_API_KEY')
+AL_SUBMIT_API_KEY = os.getenv('AL_SUBMIT_API_KEY')
 AL_USER = os.getenv('AL_USER')
 BEAVER_API_KEY = os.getenv('BEAVER_API_KEY')
 
 from assemblyline_client import get_client
 al_client = get_client("https://malware.cyber.gc.ca:443", apikey=(AL_USER, AL_API_KEY))
+al_submit_client = get_client("https://malware.cyber.gc.ca:443", apikey=(AL_USER, AL_SUBMIT_API_KEY))
 
 def beaver_search(sha256):
     response = requests.get(
@@ -36,7 +40,7 @@ def get_antivirus_service_results_from_al(sha256):
     if len(search_result['items']) > 0:
         return json.dumps([i['title_text'] for i in search_result['items'][0]['result']['sections']])
     else:
-        return "No antivirus report available for this sha256 in AssemblyLine"
+        return "AssemblyLine has not analyzed this file"
 
 def get_antivirus_service_results_from_beaver(sha256):
     response = beaver_search(sha256)
@@ -44,10 +48,25 @@ def get_antivirus_service_results_from_beaver(sha256):
         json_data = json.loads(response.text)
         if "SHADOW_SERVER" in json_data['malwareReport']['reports']:
             return json.dumps(json_data['malwareReport']['reports']['SHADOW_SERVER'][0]['results'])
-    return "No antivirus report available for this sha256 in Beaver"
+    return "Beaver has not analyzed this file"
 
+def calculateFileSha256(filename):
+    with open(filename,"rb") as f:
+        bytes = f.read() # read entire file as bytes
+        readable_hash = hashlib.sha256(bytes).hexdigest();
+        return readable_hash
+
+def submitFileToAL(filename):
+    from assemblyline_client import get_client
+    al_submit_client = get_client("https://malware.cyber.gc.ca:443", apikey=(AL_USER, AL_SUBMIT_API_KEY))
+
+    with open(filename,"rb") as f:
+        bytes = f.read()
+        submit_result = al_submit_client.submit(fname=filename, content=bytes)
+    
+    return submit_result
+    
 def chatbot():
-    llm = ChatOpenAI(temperature=0, model="gpt-3.5-turbo-1106")
     tools = [
         Tool(
             name = "GetAntivirusServiceResultsFromAssemblyLine",
@@ -58,14 +77,23 @@ def chatbot():
             name = "GetAntivirusServiceResultsFromBeaver",
             func=get_antivirus_service_results_from_beaver,
             description="useful for when you need to answer questions about Beaver's antivirus reports for a given sha256.  Beaver is a CCCS tool available at https://beaver.ops.cyber.gc.ca"
+        ),
+        Tool(
+            name = "CalculateFileSha256",
+            func=calculateFileSha256,
+            description="useful for when you need to calculate the sha256 of a file"
+        ),
+        Tool(
+            name = "SubmitFileToAssemblyLine",
+            func=submitFileToAL,
+            description="useful for when you want to submit a file, using its filename, to the malware analysis platform AssemblyLine"
         )
-]
-    mrkl = initialize_agent(tools, llm, agent=AgentType.OPENAI_FUNCTIONS, verbose=False, debug=False)
-    
-    # Create a list to store all the messages for context
-    messages = [
-    {"role": "system", "content": "You are a helpful assistant."},
     ]
+
+    memory = ConversationBufferMemory(memory_key="chat_history")
+    llm=ChatOpenAI(model="gpt-3.5-turbo-1106")
+    
+    agent_chain = initialize_agent(tools, llm, agent=AgentType.OPENAI_FUNCTIONS, verbose=True, memory=memory)
     
     # Keep repeating the following
     while True:
@@ -77,12 +105,11 @@ def chatbot():
         if message.lower() == "quit":
           break
     
-        response = mrkl.run(message)
+        response = agent_chain.run(message)
         
         # Print the response 
         print(f"Bot: {response}")
 
 if __name__ == "__main__":
     print("Start chatting with the bot (type 'quit' to stop)!")
-    print("Please note that this bot doesn't recall previous questions or answers.")
     chatbot()
